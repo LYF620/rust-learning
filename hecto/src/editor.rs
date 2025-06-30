4575dda3150efbc0b9732c3207e077dea5258c1b
LYF620 <liuyifan620@gmail.com>
2025-06-30 20:14:47 +0800

    feat: 增加panic报错且终止程序

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -1,4 +1,3 @@
-use crossterm::event;
 use crossterm::event::{read, Event::Key, KeyCode::Char};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
 
@@ -10,41 +9,25 @@ impl Editor {
     }
 
     pub fn run(&self) {
-        enable_raw_mode().unwrap();
+        if let Err(err) = self.repl() {
+            panic!("{err:#?}");
+        }
+        print!("Goodbye.\r\n");
+    }
+
+    fn repl(&self) -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match event.code {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        }
-                        _ => (),
+            if let Key(event) = read()? {
+                println!("{:?} \r", event);
+                if let Char(c) = event.code {
+                    if c == 'q' {
+                        break;
                     }
                 }
-                Err(err) => println!("Error: {}", err),
-                _ => (),
             }
         }
-        // for b in io::stdin().bytes() {
-        //     match b {
-        //         Ok(b) => {
-        //             let c = b as char;
-        //             if c.is_control() {
-        //                 println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-        //             } else {
-        //                 println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-        //             }
-
-        //             if c == 'q' {
-        //                 break;
-        //             }
-        //         }
-        //         Err(err) => println!("error: {0}", err),
-        //     }
-        // }
-        disable_raw_mode().unwrap();
+        disable_raw_mode()?;
+        Ok(())
     }
 }
